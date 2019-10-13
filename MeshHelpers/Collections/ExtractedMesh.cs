f74d62f8932c3cf589f9d10831c6770a76916044
GaryH <henhaodegaigai@gmail.com>
2019-10-13 15:02:01 -0400

    Stuff

--- MeshHelpers/Collections/ExtractedMesh.cs
+++ MeshHelpers/Collections/ExtractedMesh.cs
@@ -36,11 +36,45 @@ namespace CodeHelpers.MeshHelpers.Collections
 
 				submeshTriangles.Clear();
 			}
+
+			submeshCount = source.subMeshCount;
 		}
 
 		readonly List<Vector3> vertices;
-		readonly List<Triangle> triangles;
 		readonly List<Vector2> uvs;
+		readonly List<Triangle> triangles;
+
+		int submeshCount;
+
+		/// <summary>
+		/// Combine this mesh with the content of <paramref name="mesh"/>, and store it into this mesh
+		/// <paramref name="combineIndexedSubmeshes"/> should be true when you want to use the submesh indices from <paramref name="mesh"/>
+		/// and use it directly in this mesh. If it set to false, submesh indices will be shifted to create new submeshes
+		/// </summary>
+		public void Combine(ExtractedMesh mesh, bool combineIndexedSubmeshes = true)
+		{
+			int vertexShift = vertices.Count;
+			int submeshShift = combineIndexedSubmeshes ? 0 : submeshCount;
+
+			vertices.AddRange(mesh.vertices);
+			uvs.AddRange(mesh.uvs);
+
+			triangles.Capacity += mesh.triangles.Count;
+			submeshCount += submeshShift;
+
+			for (int i = 0; i < mesh.triangles.Count; i++)
+			{
+				var triangle = mesh.triangles[i];
+				triangles.Add
+				(
+					new Triangle
+					(
+						triangle.vertex1 + vertexShift, triangle.vertex2 + vertexShift,
+						triangle.vertex3 + vertexShift, triangle.submeshIndex + submeshShift
+					)
+				);
+			}
+		}
 
 		/**
 		 * Returns all of the unique/non-connected meshes in this mesh
@@ -49,7 +83,15 @@ namespace CodeHelpers.MeshHelpers.Collections
 		public List<ExtractedMesh> Unique()
 		{
 			var results = new List<ExtractedMesh>();
-			var uniqueVertices = new Dictionary<Vector3, int>(VertexComparer.Instance);
+			var uniqueVertices = new Dictionary<Vector3, int>(); //Hashing floating point values is fine, because we are kinda sure that they will have the exact values
+
+			for (int i = 0; i < vertices.Count; i++)
+			{
+				var vertex = vertices[i];
+				if (uniqueVertices.ContainsKey(vertex)) continue;
+
+				uniqueVertices.Add(vertex, i);
+			}
 
 			throw new NotImplementedException();
 		}
@@ -65,11 +107,11 @@ namespace CodeHelpers.MeshHelpers.Collections
 				this.submeshIndex = submeshIndex;
 			}
 
-			readonly int vertex1;
-			readonly int vertex2;
-			readonly int vertex3;
+			public readonly int vertex1;
+			public readonly int vertex2;
+			public readonly int vertex3;
 
-			readonly int submeshIndex;
+			public readonly int submeshIndex;
 		}
 	}
 }
