388ddb60527ccb4f02f680b82f48aed0e99f9412
Gary <henhaodegaigai@gmail.com>
2020-06-28 23:20:21 -0400

    reduced WeakEventHandler allocation

--- Events/WeakEventHandler.cs
+++ Events/WeakEventHandler.cs
@@ -6,12 +6,13 @@ namespace CodeHelpers.Events
 	{
 		/// <summary>
 		/// <paramref name="invocationAction"/> should invoke the method on <paramref name="listener"/> that you want to get invoked when the event is raised.
-		/// NOTE: Remember to subscribe <see cref="OnEvent"/> to the event after you created this handler.
+		/// NOTE: Remember to subscribe <see cref="eventInput"/> to the event after you created this handler.
 		/// </summary>
 		public WeakEventHandler(TListener listener, Action<TListener> invocationAction)
 		{
 			listenerReference = new WeakReference<TListener>(listener);
 			this.invocationAction = invocationAction;
+			eventInput = OnEvent;
 		}
 
 		readonly WeakReference<TListener> listenerReference;
@@ -19,12 +20,14 @@ namespace CodeHelpers.Events
 
 		Action unsubscribeAction;
 		bool unsubscribed;
-		
+
 		/// <summary>
-		/// You should never invoke this method yourself.
-		/// Instead, register/subscribe this method to the event.
+		/// You should never invoke this action yourself.
+		/// Instead, register/subscribe this action to the event.
 		/// </summary>
-		public void OnEvent()
+		public readonly Action eventInput;
+		
+		void OnEvent()
 		{
 			if (unsubscribeAction == null) throw new Exception($"You must set an {nameof(unsubscribeAction)} when initializing this handler.");
 			if (unsubscribed) throw new Exception($"The handler should already be unsubscribed but the event is still reaching it! You did not properly define the {nameof(unsubscribeAction)}");
@@ -42,7 +45,7 @@ namespace CodeHelpers.Events
 
 		/// <summary>
 		/// Register an action that will be invoked sometime after the listener got garbage collected.
-		/// This action must unsubscribe this handler's <see cref="OnEvent"/> method from the event so it is never invoked again.
+		/// This action must unsubscribe this handler's <see cref="eventInput"/> action from the event so it is never invoked again.
 		/// </summary>
 		public void SetUnsubscribeAction(Action action)
 		{
