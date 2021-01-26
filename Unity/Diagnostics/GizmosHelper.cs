1eeffc329692a1b91fbc533f60b44d32d368c780
MMXXXVIII <henhaodegaigai@gmail.com>
2021-01-25 23:52:38 -0500

    Added one more gizmo

--- Unity/Diagnostics/GizmosHelper.cs
+++ Unity/Diagnostics/GizmosHelper.cs
@@ -7,9 +7,9 @@ namespace CodeHelpers.Unity.Diagnostics
 {
 	public static class GizmosHelper
 	{
-		public static void DrawArrow(Float3 origin, Float3 end, float angle = 30f) => DrawArrow(origin, end, (origin - end).Magnitude / 10f, angle);
+		public static void DrawArrow(Float3 origin, Float3 end) => DrawArrow(origin, end, (origin - end).Magnitude / 10f);
 
-		public static void DrawArrow(Float3 origin, Float3 end, float arrowSize, float angle)
+		public static void DrawArrow(Float3 origin, Float3 end, float length)
 		{
 			Gizmos.DrawLine(origin, end);
 
@@ -22,9 +22,29 @@ namespace CodeHelpers.Unity.Diagnostics
 
 			void DrawPiece(Float3 perpendicular)
 			{
-				var localRotation = Quaternion.AngleAxis(angle, Float3.Cross(Float3.up, perpendicular));
-				Gizmos.DrawLine(end, (Vector3)end + rotation * localRotation * Float3.up * arrowSize);
+				const float Angle = 30f;
+
+				var localRotation = Quaternion.AngleAxis(Angle, Float3.Cross(Float3.up, perpendicular));
+				Gizmos.DrawLine(end, (Vector3)end + rotation * localRotation * Float3.up * length);
 			}
+		}
+
+		public static void DrawAnchor(Transform transform) => DrawAnchor(transform.position, transform.up, transform.forward);
+
+		public static void DrawAnchor(Float3 origin, Float3 up, Float3 forward)
+		{
+			up = up.Normalized;
+			forward = forward.Normalized;
+			Float3 right = up.Cross(forward);
+
+			Gizmos.color = Color.red;
+			DrawArrow(origin, origin + right);
+
+			Gizmos.color = Color.green;
+			DrawArrow(origin, origin + up);
+
+			Gizmos.color = Color.blue;
+			DrawArrow(origin, origin + forward);
 		}
 	}
 }
