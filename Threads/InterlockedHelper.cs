f38705451d3022c02a9e1d12ab98d613ff2aa919
MMXXXVIII <henhaodegaigai@gmail.com>
2021-03-14 19:55:57 -0400

    Added Interlocked Max

--- Threads/InterlockedHelper.cs
+++ Threads/InterlockedHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace CodeHelpers.Threads
 {
@@ -12,29 +13,141 @@ namespace CodeHelpers.Threads
 
 		public static float Add(ref float location, float value)
 		{
-			float newCurrentValue = location;
+			float nextValue = location;
 
 			while (true)
 			{
-				float currentValue = newCurrentValue;
+				float currentValue = nextValue;
 				float newValue = currentValue + value;
 
-				newCurrentValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
-				if (newCurrentValue == currentValue) return newValue;
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
 			}
 		}
 
 		public static double Add(ref double location, double value) //NOTE: currently does not correctly handle NaNs
 		{
-			double newCurrentValue = location;
+			double nextValue = location;
 
 			while (true)
 			{
-				double currentValue = newCurrentValue;  //The potential value if no other thread changed location
-				double newValue = currentValue + value; //The potential result after the addition if location is unchanged
+				double currentValue = nextValue;        //The potential initial value if no other thread changed location
+				double newValue = currentValue + value; //The potential result after the addition if no other thread changes location
 
-				newCurrentValue = Interlocked.CompareExchange(ref location, newValue, currentValue); //Only change location if it is unchanged
-				if (newCurrentValue == currentValue) return newValue;                                //If exchange was successful, addition finished
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue); //Only change location if it is unchanged
+				if (nextValue == currentValue) return newValue;                                //If exchange was successful, addition finished
+			}
+		}
+
+		public static int Max(ref int location, int other)
+		{
+			int nextValue = location;
+
+			while (true)
+			{
+				int currentValue = nextValue;
+				int newValue = Math.Max(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
+			}
+		}
+
+		public static float Max(ref float location, float other)
+		{
+			float nextValue = location;
+
+			while (true)
+			{
+				float currentValue = nextValue;
+				float newValue = Math.Max(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
+			}
+		}
+
+		public static long Max(ref long location, long other)
+		{
+			long nextValue = location;
+
+			while (true)
+			{
+				long currentValue = nextValue;
+				long newValue = Math.Max(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
+			}
+		}
+
+		public static double Max(ref double location, double other)
+		{
+			double nextValue = location;
+
+			while (true)
+			{
+				double currentValue = nextValue;
+				double newValue = Math.Max(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
+			}
+		}
+
+		public static int Min(ref int location, int other)
+		{
+			int nextValue = location;
+
+			while (true)
+			{
+				int currentValue = nextValue;
+				int newValue = Math.Min(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
+			}
+		}
+
+		public static float Min(ref float location, float other)
+		{
+			float nextValue = location;
+
+			while (true)
+			{
+				float currentValue = nextValue;
+				float newValue = Math.Min(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
+			}
+		}
+
+		public static long Min(ref long location, long other)
+		{
+			long nextValue = location;
+
+			while (true)
+			{
+				long currentValue = nextValue;
+				long newValue = Math.Min(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
+			}
+		}
+
+		public static double Min(ref double location, double other)
+		{
+			double nextValue = location;
+
+			while (true)
+			{
+				double currentValue = nextValue;
+				double newValue = Math.Min(currentValue, other);
+
+				nextValue = Interlocked.CompareExchange(ref location, newValue, currentValue);
+				if (nextValue == currentValue) return newValue;
 			}
 		}
 	}
