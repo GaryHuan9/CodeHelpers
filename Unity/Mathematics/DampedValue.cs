15e027b59b7d0c98f1534b4b79b44d5fc8306dfa
MMXXXVIII <henhaodegaigai@gmail.com>
2021-06-08 17:53:18 -0400

    Added modes to DampedValue

--- Unity/Mathematics/DampedValue.cs
+++ Unity/Mathematics/DampedValue.cs
@@ -30,9 +30,24 @@ namespace CodeHelpers.Unity.Mathematics
 			}
 		}
 
-		public float Velocity => _velocity;
+		public float Velocity
+		{
+			get => _velocity;
+			private set => _velocity = value;
+		}
 
+		[field: SerializeField] public DampedMode Mode { get; set; } = DampedMode.damp;
+
+		/// <summary>
+		/// The <see cref="Target"/> of this <see cref="DampedValue"/> to advance <see cref="Current"/> towards.
+		/// </summary>
 		[field: SerializeField] public float Target { get; set; }
+
+		/// <summary>
+		/// This property performs differently under different <see cref="Mode"/>. If <see cref="Mode"/> is <see cref="DampedMode.damp"/>,
+		/// this value indicates the approximated time for <see cref="Current"/> to arrive at <see cref="Target"/>. If <see cref="Mode"/>
+		/// is <see cref="DampedMode.linear"/>, this value indicates the time it takes for <see cref="Current"/> to navigate a distance of 1.
+		/// </summary>
 		[field: SerializeField] public float SmoothTime { get; set; }
 
 		/// <summary>
@@ -47,13 +62,53 @@ namespace CodeHelpers.Unity.Mathematics
 
 		public void Update(float deltaTime)
 		{
+			float current;
+
+			switch (Mode)
+			{
+				case DampedMode.damp:
+				{
+					current = Scalars.Damp(Current, Target, ref _velocity, SmoothTime, deltaTime);
+					break;
+				}
+				case DampedMode.linear:
+				{
+					float maxDelta = deltaTime / SmoothTime;
+					float distance = Target - Current;
+
+					if (Math.Abs(distance) <= maxDelta) current = Target;
+					else current = Current + maxDelta * Math.Sign(distance);
+
+					CalculateVelocity();
+
+					break;
+				}
+				case DampedMode.instant:
+				{
+					current = Target;
+					CalculateVelocity();
+
+					break;
+				}
+				default: throw ExceptionHelper.Invalid(nameof(Mode), Mode, InvalidType.unexpected);
+			}
+
 			Changed = false;
-			Current = Scalars.Damp(Current, Target, ref _velocity, SmoothTime, deltaTime);
+			Current = current;
+
+			void CalculateVelocity() => Velocity = (current - Current) / deltaTime;
 		}
 
 		public static void Update(float deltaTime, params DampedValue[] values)
 		{
 			for (int i = 0; i < values.Length; i++) values[i].Update(deltaTime);
 		}
+	}
+
+	public enum DampedMode : byte
+	{
+		damp,
+		linear,
+		instant
 	}
 }
